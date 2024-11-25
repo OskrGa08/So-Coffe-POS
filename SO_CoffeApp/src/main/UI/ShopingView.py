82c00b469f61f5e5ddca37f81f85df6247db2936
OskrGa08 <Bunbueygasp@gmail.com>
2024-11-24 19:37:40 -0600

    Damn

--- SO_CoffeApp/src/main/UI/ShopingView.py
+++ SO_CoffeApp/src/main/UI/ShopingView.py
@@ -6,64 +6,69 @@ from tkinter import messagebox
 from tkinter import ttk
 
 # Configuration main window---------------------------
-sells = Tk()
-sells.geometry("1440x700")
-sells.minsize(700, 500)
-sells.maxsize(1500, 580)
-sells.configure(bg="white")
-sells.title("Gestion de ventas")
+sh = Tk()
+sh.geometry("1220x700")
+sh.minsize(700, 500)
+sh.maxsize(1500, 580)
+sh.configure(bg="white")
+sh.title("Gestion de Compras")
 
 #Top Bar-----------------------------------------------
-topBar_frame = Frame(sells, bg="#CE7710")
+topBar_frame = Frame(sh, bg="#CE7710")
 topBar_frame.place(x=0, y=0, relwidth=1, height=30)
 
 #Frames--------------------------------------------------
-detail_Frame = Frame(sells, bg="#D2D2D2")
-detail_Frame.place(x=1040, y=60, width=340, height=460)
+detail_Frame = Frame(sh, bg="#D2D2D2")
+detail_Frame.place(x=850, y=60, width=340, height=460)
 
 #Logica de los command para que habra resectivas ventanas cada opcion(abrir las ventanas respectivas a cada gestionar)---------------------------------------
 def managEmployees():
-    sells.destroy()
+    sh.destroy()
     from Employees import Employees
     Employees()
 
 def managInputs():
-    sells.destroy()
+    sh.destroy()
     from Inputs import Inputs
     Inputs()
 
 def pontiOfSale():
-    sells.destroy()
+    sh.destroy()
     from POS import POS 
     POS()
 
 def managPosition():
-    sells.destroy()
+    sh.destroy()
     from Position import Position
     Position()
 
 def managProductCategory():
-    sells.destroy()
+    sh.destroy()
     from ProductCategory import ProductCategory
     ProductCategory()
 
 def managProducts():
-    sells.destroy()
+    sh.destroy()
     from Products import Products 
     Products()
 
+def managSells():
+    sh.destroy()
+    from Sells import Sells 
+    Sells()
+
 def managOutPuts():
-    sells.destroy()
+    sh.destroy()
     from OutPuts import OutPuts 
     OutPuts()
 
 def managShopping():
-    sells.destroy()
+    sh.destroy()
     from Shopping import Shopping
     Shopping()
 
 def  managSupplier():
-    sells.destroy()
+    sh.destroy()
     from Suppliers import Suppliers 
     Suppliers()
 
@@ -82,55 +87,55 @@ MenuButton_barFrame.menu.add_command(label="Gestion de Productos", foreground="w
 MenuButton_barFrame.menu.add_command(label="Gestion de Salidas", foreground="white", font=("New Times Roman", 12), command=managOutPuts)
 MenuButton_barFrame.menu.add_command(label="Gestion de Compras", foreground="white", font=("New Times Roman", 12), command=managShopping)
 MenuButton_barFrame.menu.add_command(label="Gestion de Proveedores", foreground="white", font=("New Times Roman", 12), command=managSupplier)
+MenuButton_barFrame.menu.add_command(label="Gestion de Ventas", foreground="white", font=("New Times Roman", 12), command=managSells)
 MenuButton_barFrame["menu"]= MenuButton_barFrame.menu
 
-Main_Label = Label(sells, text="VENTAS", fg="black", bg="white", font=("Arial Black", 18))
-Main_Label.place(x=430, y=55)
+Main_Label = Label(sh, text="COMPRAS", fg="black", bg="white", font=("Arial Black", 18))
+Main_Label.place(x=350, y=75)
 
-# Table to display products
-sells_columns = ("ID Venta", "Empleado", "Fecha", "Hora", "Total")
-sells_tree = ttk.Treeview(sells, columns=sells_columns, show="headings", height=5)
-for col in sells_columns:
-    sells_tree.heading(col, text=col)
-sells_tree.place(x=20, y=160, height=300)
+# Table to display 
+sh_columns = ("ID Compra", "Proveedor", "Fecha", "Total")
+sh_tree = ttk.Treeview(sh, columns=sh_columns, show="headings", height=5)
+for col in sh_columns:
+    sh_tree.heading(col, text=col)
+sh_tree.place(x=20, y=160, height=300)
 
-sells_scrollbar = Scrollbar(sells, orient="vertical", command=sells_tree.yview)
-sells_scrollbar.place(x=1002, y=165, height=290)
-sells_tree.configure(yscrollcommand=sells_scrollbar.set)
+sh_scrollbar = Scrollbar(sh, orient="vertical", command=sh_tree.yview)
+sh_scrollbar.place(x=802, y=165, height=290)
+sh_tree.configure(yscrollcommand=sh_scrollbar.set)
 
 #connection bd---------------------------------------------------------------------------------------------------------------------
 def get_db_connection():
     conn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-M8N9242;DATABASE=Socoffe;UID=sa;PWD=sistemas123;')
     return conn
 
-def load_Sells():
+def load_Sh():
     try:
-        sells_tree.delete(*sells_tree.get_children())
+        sh_tree.delete(*sh_tree.get_children())
         conn = get_db_connection()
         cursor = conn.cursor()
-        cursor.execute("EXEC MostrarVentas") #el metodo no imprime el noombre de ahi en mas todo bien 
+        cursor.execute("EXEC mostrar_compras")
         rows = cursor.fetchall()
         for row in rows:
             formatted_row = (
                 row[0], 
                 row[1],
                 row[2],
-                row[3],
-                f"{row[4]}")
-            sells_tree.insert("", "end", values=formatted_row)
+                f"{row[3]}")
+            sh_tree.insert("", "end", values=formatted_row)
         cursor.close()
         conn.close()
     except Exception as e:
-        messagebox.showerror("Error", f"Error al cargar las ventas: {e}")
-load_Sells()
+        messagebox.showerror("Error", f"Error al cargar las compras: {e}")
+load_Sh()
 
 def on_select_output(event):
     # Obtener el ID de la salida seleccionada
-    selected_item = sells_tree.selection()
+    selected_item = sh_tree.selection()
     if not selected_item:
         return  # Si no hay selección, salir
 
-    item = sells_tree.item(selected_item)
+    item = sh_tree.item(selected_item)
     venta_id = item["values"][0]  # El ID está en la primera columna
 
     # Limpiar el frame de detalles
@@ -143,22 +148,22 @@ def on_select_output(event):
         cursor = conn.cursor()
         
         # Ejecutar el procedimiento almacenado con el ID de la venta
-        cursor.execute("EXEC ObtenerDetallesVenta ?", venta_id)
+        cursor.execute("EXEC obtenerDetalleCompra ?", venta_id)
         detalles = cursor.fetchall()
         
         # Mostrar los detalles en el frame
-        Label(detail_Frame, text="Detalles de la Venta", bg="#D2D2D2", font=("Arial Black", 10)).pack(pady=5)
+        Label(detail_Frame, text="Detalles de la Compra", bg="#D2D2D2", font=("Arial Black", 10)).pack(pady=5)
         
         for detalle in detalles:
-            texto = f"Nombre: {detalle[1]}, Costo: {detalle[2]}, Cantidad: {detalle[3]}, Importe: {detalle[4]}"
+            texto = f"Insumo: {detalle[1]}, Cantidad: {detalle[2]}, Costo: {detalle[3]}"
             Label(detail_Frame, text=texto, bg="#D2D2D2", anchor="w").pack(fill="x", padx=10, pady=2)
         
         cursor.close()
         conn.close()
     except Exception as e:
-        messagebox.showerror("Error", f"Error al cargar los detalles de la venta: {e}")
+        messagebox.showerror("Error", f"Error al cargar los detalles de la compra: {e}")
 
 # Vincular el evento de selección al treeview
-sells_tree.bind("<<TreeviewSelect>>", on_select_output)
+sh_tree.bind("<<TreeviewSelect>>", on_select_output)
 
-sells.mainloop()
+sh.mainloop()
